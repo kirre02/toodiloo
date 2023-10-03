b4942dcd6cd034572778631bdc4bbbed02689d88
kirre02 <kirre02@outlook.com>
2023-10-03 16:05:17 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "terraform-tfe-workspace";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_20
+            ];
+          };
+        };
+      };
+    };
+}
