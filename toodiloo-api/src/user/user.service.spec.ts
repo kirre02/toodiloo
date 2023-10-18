3e38dedef3604ada41f1bab4849bcddcceab3027
kirre02 <kirre02@outlook.com>
2023-10-18 16:34:11 +0200

    feat(test): inital work on tests

--- toodiloo-api/src/user/user.service.spec.ts
+++ toodiloo-api/src/user/user.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+const username = 'test';
+const usermail = 'test@test.io';
+const user1 = 1;
+const user2 = 2;
+
+const userArray = [
+  { username: 'Erik', email:'kirre@test.io', id: user1 },
+  { username: 'Fabricio', email: 'fabri@test.io', id: user2 },
+];
+
+const oneUser = userArray[0];
+
+const db = {
+  user: {
+    findMany: jest.fn().mockResolvedValue(userArray),
+    findUnique: jest.fn().mockResolvedValue(oneUser),
+    findFirst: jest.fn().mockResolvedValue(oneUser),
+    create: jest.fn().mockReturnValue(oneUser),
+    save: jest.fn(),
+    update: jest.fn().mockResolvedValue(oneUser),
+    delete: jest.fn().mockResolvedValue(oneUser),
+  },
+};
 
 describe('UserService', () => {
   let service: UserService;
+  let prisma: PrismaService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService, {
+          provide: PrismaService,
+          useValue: db,
+        }
+      ],
     }).compile();
-
+  
     service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create new user', async () => {
+    expect(
+      service.createUser({
+        name: username,
+        email: usermail,
+        password: "test123"
+      })
+    )
   });
 });
--- toodiloo-api/src/user/user.service.ts
+++ toodiloo-api/src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { User, Prisma as db } from '@prisma/client';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private db: PrismaService) {}
+
+  async user(uniqeUser: db.UserWhereUniqueInput): Promise<User | null> {
+    return this.db.user.findUnique({
+      where: uniqeUser,
+    });
+  }
+
+  async createUser(data: db.UserCreateInput): Promise<User> {
+    return this.db.user.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: db.UserWhereUniqueInput;
+    data: db.UserUpdateInput;
+  }): Promise<User> {
+    const { data, where } = params;
+    return this.updateUser({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: db.UserWhereUniqueInput): Promise<User> {
+    return this.db.user.delete({
+      where,
+    });
+  }
+}
